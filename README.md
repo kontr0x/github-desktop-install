59e7b5d1320ac7c6753a279718393a6c819088ff
Max <55395652+kontr0x@users.noreply.github.com>
2023-03-01 11:09:44 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# GitHub desktop installation script
+
+This script will download the most recent GitHub desktop version for ur Linux and, if desired, has the ability to install it as well. If your system does not use deb or rpm packages, it will fall back to the available AppImage.
+
+Tested the script on:
+- ubuntu
+- fedora
+
+The project came to life as a result of [berkorbay](https://gist.github.com/berkorbay)'s presumably discontinued gist page, which can be viewed [here](https://gist.github.com/berkorbay/6feda478a00b0432d13f1fc0a50467f1).
+
+The script will be updated to remove all the todos. Please feel free to contribute.
---
+++ installGitHubDesktop.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+installed=0
+installed_version="n.A."
+
+if [ -x "$(command -v dpkg)" ]; then
+  package_manager="deb"
+  if dpkg -s "github-desktop" &> /dev/null; then
+    installed=1
+    installed_version=$(dpkg -s github-desktop | grep -o -E "Version: [0-9.]*-linux1" | head -n 1 | grep -o -E "[0-9.]*\.[0-9]")
+  fi
+elif [ -x "$(command -v rpm)" ]; then
+  package_manager="rpm"
+  if rpm -q github-desktop &> /dev/null; then
+    installed=1
+    installed_version=$(rpm -q github-desktop | grep -o -E "[0-9.]*\.[0-9]")
+  fi
+else
+  package_manager="AppImage"
+fi
+
+if [ $installed -eq 1 ]; then
+  echo Another version of Github desktop is already installed \($installed_version\)
+  exit 0
+  # TODO: implement update functionality
+else
+  release_url="https://github.com/shiftkey/desktop/releases/latest"
+  
+  # Extract the link which contains the actual download links
+  expanded_assets_url=$(curl -sL $release_url | grep -o -E "https:\/\/github.com\/shiftkey\/desktop\/releases\/expanded_assets\/release-[0-9.]*-linux1")
+  
+  download_url=$(curl -sL $expanded_assets_url | grep -o -E "\/shiftkey\/desktop\/releases\/download\/release-[0-9.]*-linux1\/GitHubDesktop-linux-.*-linux1\.$package_manager")
+  
+  filename=$(grep -o -E "GitHubDesktop-linux-[0-9.]*-linux1\.[a-zA-Z]{3,8}" <<< $download_url)
+  echo "Downloading $filename ..."
+  # TODO: add check if file is already present
+  curl -sLOJ https://github.com$download_url
+  echo "Download complete!"
+  
+  read -p "Should the downloaded package be installed? (y/n)" choice
+  # TODO: add redundancy check if the package is present in the local directory
+  
+  case "$choice" in
+    y|Y ) :;;
+    n|N ) exit 0;;
+    * ) echo "Invalid input";;
+  esac
+  
+  if [ $package_manager == "deb" ]; then
+    echo "Installing package, this requires sudo privileges!"
+    sudo dpkg -i ./$filename && echo "Package installed successfully"
+  elif [ $package_manager == "rpm" ]; then
+    echo "Installing package, this requires sudo privileges!"
+    sudo rpm -i ./$filename && echo "Package installed successfully"
+  else
+    echo "Adding execute permissions on AppImage"
+    chmod +x ./$filename && echo "The app can now be started by execute the AppImage"
+  fi
+fi
+
+exit 0
+
